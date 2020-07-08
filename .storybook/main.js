e78211c615821439a951722481029b91e19cfb34
Madeleine Olson <madeleineolson@outlook.com>
2020-07-08 23:33:17 +0200

    Added storybook

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ['../src/**/*.stories.(js|mdx|tsx)'],
+  addons: [
+    {
+       name: '@storybook/preset-create-react-app',
+    },
+    {
+       name: '@storybook/addon-actions',
+    },
+    {
+       name: '@storybook/addon-links',
+    },
+    {
+       name: '@storybook/addon-docs',
+       options: { configureJSX: true }
+    }
+  ]
+};
