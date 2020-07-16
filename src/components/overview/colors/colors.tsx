396eb40cc9d50cfb72ab33eaa99c69580c883a5f
Madeleine Olson <madeleineolson@outlook.com>
2020-07-16 15:37:11 +0200

    Added color variables

--- src/components/overview/colors/colors.tsx
+++ src/components/overview/colors/colors.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { colors } from "../../../styles/colors";
-
-interface ColorsProps {
-  color: string;
-}
+import "./colors.scss";
 
 export default function Colors() {
   const colorItems = colors.map((color) => (
-    <li>
-      {color.name} - {color.colorCode}
-    </li>
+    <div className="variableColors__item">
+      <div
+        className="variableColors__preview"
+        style={{ backgroundColor: color.colorCode }}
+      ></div>
+      <span className="variableColors__info">{color.name}</span>
+      <span className="variableColors__info">{color.colorCode}</span>
+    </div>
   ));
-  return <ul>{colorItems}</ul>;
+  return (
+    <div className="variableColors">
+      <h1>Colors</h1>
+      <p>These are the color variables that are used in the project.</p>
+      <hr />
+      <div className="variableColors__items">{colorItems}</div>
+    </div>
+  );
 }
--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -3,9 +3,9 @@
 //
 
 export const colors = [
-  { name: "colorPrimary", colorCode: "#d95b72" },
-  { name: "colorPrimaryLight", colorCode: "#ff6b86" },
-  { name: "colorSecondary", colorCode: "#3f3b59" },
-  { name: "colorBlack", colorCode: "#000" },
-  { name: "colorWhite", colorCode: "#fff" },
+  { name: "$color-primary", colorCode: "#d95b72" },
+  { name: "$color-primary-light", colorCode: "#ff6b86" },
+  { name: "$color-secondary", colorCode: "#3f3b59" },
+  { name: "$color-black", colorCode: "#000" },
+  { name: "$color-white", colorCode: "#fff" },
 ];
